7d8ef6f9b2274fd1744d94aee684c70a81df5f83
Jordan Zucker <jordanz@pubnub.com>
2022-05-01 16:02:34 -0700

    Simple element class, it's enough for now

---
+++ schoonmaker/element.py
@@ -0,0 +1,39 @@
+from .utils import get_logger
+from .element_tag import ElementTag
+
+
+log = get_logger(__name__)
+
+
+class Element(object):
+    def __init__(self, xml_element):
+        self.xml_element = xml_element
+
+    @property
+    def tag(self):
+        return self.xml_element.tag
+
+    @property
+    def tag_enum(self):
+        return ElementTag(self.tag)
+
+    @property
+    def is_content(self):
+        return bool(self.tag_enum == ElementTag.CONTENT)
+
+    @property
+    def attrib(self):
+        return self.xml_element.attrib
+
+    @property
+    def text(self):
+        return self.xml_element.text
+
+    def __repr__(self):
+        return f'Element => ' \
+               f'tag: {self.tag}, ' \
+               f'attrib: {self.attrib}'
+
+    @staticmethod
+    def test():
+        log.info('element -> test!')
--- schoonmaker/parser.py
+++ schoonmaker/parser.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 from .utils import get_logger
 from .element_tag import ElementTag
+from .element import Element
 
 
 log = get_logger(__name__)
@@ -18,18 +19,24 @@ class Parser(object):
     def parse_content(self, element):
         log.info('!!!!!! we are parsing content!!!')
         log.info(f'Content element: {element} has text: {element.text}')
+        el = Element(element)
+        log.info(f'el: {el}')
         for nested_element in element:
             n_m = f'nested_element: {nested_element} with ' \
                   f'tag: {nested_element.tag}, ' \
                   f'attrib: {nested_element.attrib}, ' \
                   f'text: {nested_element.text}'
             log.info(n_m)
+            nested_el = Element(nested_element)
+            log.info(f'nested_el: {nested_el}')
             for double_nested_element in nested_element:
                 d_m = f'double_nested_element: {double_nested_element} ' \
                       f'with tag: {double_nested_element.tag}, ' \
                       f'attrib: {double_nested_element.attrib}, ' \
                       f'text: {double_nested_element.text}'
                 log.info(d_m)
+                double_nested_el = Element(double_nested_element)
+                log.info(f'double_nested_el: {double_nested_el}')
 
     def naive_parse(self, file_name):
         log.info(f'going with file_name: {file_name}')
@@ -40,11 +47,15 @@ class Parser(object):
               f'tag: {root.tag}, ' \
               f'attrib: {root.attrib}'
         log.info(r_m)
+        root_element = Element(root)
+        log.info(f'root_element: {root_element}')
         for child in root:
             c_m = f'child: {child} with ' \
                   f'tag: {child.tag}, ' \
                   f'attrib: {child.attrib}'
             log.info(c_m)
+            child_element = Element(child)
+            log.info(f'child_element: {child_element}')
             if child.tag == ElementTag.CONTENT.value:
                 log.info('we found content!!!')
                 log.info(f'Content child: {child} has text: {child.text}')
