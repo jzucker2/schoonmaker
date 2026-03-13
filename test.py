afc4771a7598bc358a486d8bf6e1e4260294665d
Jordan Zucker <jordan.zucker@gmail.com>
2026-03-13 15:01:56 -0700

    What am I even doing

--- test.py
+++ test.py
@@ -10,17 +10,17 @@ log = get_logger(__name__)
 def run():
     parser = Parser()
     parser.test()
-    file_name = 'samples/final_draft_12_sample.fdx'
-    log.info(f'hardcoded file_name: {file_name}')
+    file_name = "samples/final_draft_12_sample.fdx"
+    log.info(f"hardcoded file_name: {file_name}")
     parser.naive_parse(file_name)
-    log.info('done with `run`')
+    log.info("done with `run`")
 
 
 def main():
-    log.info('Begin `main` function')
+    log.info("Begin `main` function")
     run()
-    log.info('end of `main` function')
+    log.info("end of `main` function")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
